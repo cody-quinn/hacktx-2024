7e6093314bbd71993585ab10372aaa08895a04f2
Nic Gaffney <gaffney_nic@protonmail.com>
2024-11-02 18:34:16 -0500

    feat: ROMS can now be loaded into a room

--- app/src/routers/rooms.py
+++ app/src/routers/rooms.py
@@ -1,10 +1,33 @@
 import threading
+import asyncio
+from pathlib import Path
 from fastapi import APIRouter, WebSocket
 from pydantic import BaseModel
 from src.emulator import Emulator
 from collections import Counter
 
 router = APIRouter()
+
+class Rom(BaseModel):
+  id: str
+  art: str
+  title: str
+
+class RomPrivate(Rom):
+  filename: str
+
+roms = {
+  "tetris": RomPrivate(
+    id="tetris",
+    art="/assets/tetris.png",
+    title="Tetris(R)",
+    filename="./app/games/tetris.gb",
+  )
+}
+
+@router.get("/roms", response_model=list[Rom])
+def get_roms() -> list[RomPrivate]:
+  return list(roms.values())
 
 def get_id():
   id = 0
@@ -42,13 +65,16 @@ class Room:
   players: list[Player]
   inputs: dict[int,str]
 
-  def __init__(self, rom: str):
+  def __init__(self, rom: RomPrivate):
+    self.running = False
     self.id = get_room_id()
-    self.emulator = Emulator(rom)
+    self.emulator = Emulator(rom.filename)
     self.players = []
     self.inputs = {}
 
   def player_join(self, player: Player):
+    if player in self.players:
+      return
     self.players.append(player)
 
   def player_leave(self, player: Player):
@@ -56,7 +82,7 @@ class Room:
 
   def run(self):
     self.emulator.start_game()
-    self.thread = threading.Thread(target = self.run_internal)
+    self.thread = threading.Thread(target = asyncio.run, args = (self.run_internal(), ))
     self.thread.start()
 
   async def run_internal(self):
@@ -87,13 +113,16 @@ class Room:
 
 rooms: list[Room] = []
 
+class RoomCreate(BaseModel):
+  rom_id: str
+
 @router.get("/rooms", response_model=list[int])
 async def get_rooms() -> list[int]:
   return [room.id for room in rooms]
 
-@router.post("/rooms/{rom}")
-async def create_room(rom: str):
-  room = Room(rom)
+@router.post("/rooms")
+async def create_room(body: RoomCreate):
+  room = Room(roms[body.rom_id])
   rooms.append(room)
   room.run()
   return room.id
@@ -114,7 +143,7 @@ async def websocket(sock: WebSocket, room_id: int):
   await sock.accept()
   player = Player(sock)
 
-  data = await player.sock.receive_text()
-  player.nick = data
-
+  player.nick = str(await sock.receive())
   room.player_join(player)
+
+  print(f"Player named {player.nick} joined room {room_id}.")
