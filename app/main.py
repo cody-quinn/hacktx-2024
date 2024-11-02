4e1f57eea1dae7a0c401b5f65636cfa6f0fb5d79
Cody <cody@codyq.dev>
2024-11-02 15:48:57 -0700

    feat: fix nic's stupid idiotic moronic dumbass mistake

--- app/main.py
+++ app/main.py
@@ -8,10 +8,13 @@ from starlette.responses import Response
 from starlette.types import Scope
 
 from src.database import initialize_db
+from src.routers import rooms
 
 debug = os.getenv("DEBUG", "false").lower() == "true"
 
 api = FastAPI(debug=debug)
+
+api.include_router(rooms.router)
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
