9c5793ab699d4f71467fb601e775aff0800c356e
Cody <cody@codyq.dev>
2024-11-02 18:40:25 -0700

    Yippeeee

--- app/src/emulator/__init__.py
+++ app/src/emulator/__init__.py
@@ -3,33 +3,36 @@ import pyboy as pb
 class Emulator:
   game: pb.PyBoy
   rom: str
-  fcount: int = 0
+
+  framecount: int = 0
+  framebuffer: bytes
 
   def __init__(self, rom: str):
+    self.rom = rom
     self.game = pb.PyBoy(rom)
 
-  def start_game(self):
-    self.game.game_wrapper.start_game()
-    self.game.set_emulation_speed(1)
-
-  def tick(self):
-    self.game.tick()
-    self.fcount += 1
+  def start(self):
+    self.game.game_wrapper.start_game(timer_div=0x00)
+    # self.game.set_emulation_speed(0)
 
   def stop(self):
     self.game.stop()
 
+  def tick(self):
+    self.game.tick()
+    self.framecount += 1
+
   def send_button(self, cmd: str):
-    if self.fcount % 2 == 0:
+    if self.framecount % 2 == 0:
       self.game.button(cmd)
 
-  def get_frame(self):
-    raw = bytes([int(x/255 * 3) for x in self.game.screen.ndarray.flatten()[::4]])
-    compressed = []
-    for i in range(len(raw) % 4):
-      n0 = raw[i]   << 6
-      n1 = raw[i+1] << 4
-      n2 = raw[i+2] << 2
-      n3 = raw[i+3]
-      compressed[i] = n0 | n1 | n2 | n3
-    return compressed
+  def update_framebuffer(self):
+    raw = self.game.screen.ndarray.flatten()
+    buf = [0] * (len(raw) // 16 + 1)
+    for i, j in enumerate(range(0, len(raw), 16)):
+      n0 = raw[j]    // 85 << 6
+      n1 = raw[j+4]  // 85 << 4
+      n2 = raw[j+8]  // 85 << 2
+      n3 = raw[j+12] // 85
+      buf[i] = n0 | n1 | n2 | n3
+    self.framebuffer = bytes(buf)
--- app/src/routers/rooms.py
+++ app/src/routers/rooms.py
@@ -1,7 +1,8 @@
+import concurrent.futures
 import threading
 import asyncio
 from pathlib import Path
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect 
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
 from src.emulator import Emulator
 from collections import Counter
@@ -21,7 +22,7 @@ roms = {
     id="tetris",
     art="/assets/tetris.png",
     title="Tetris(R)",
-    filename="./app/games/tetris.gb",
+    filename="app/games/tetris.gb",
   )
 }
 
@@ -29,46 +30,47 @@ roms = {
 def get_roms() -> list[RomPrivate]:
   return list(roms.values())
 
-def get_id():
-  id = 0
-  def inner():
-    nonlocal id
-    id = id + 1
-    return id
-  return inner
+def id_generator():
+  next_id = 0
+  while True:
+    next_id += 1
+    yield next_id
 
-get_room_id = get_id()
-get_player_id = get_id()
+room_id = id_generator()
+player_id = id_generator()
 
 class Player:
-  nick: str
-  id: int
   sock: WebSocket
+
+  id: int
+  nick: str
 
   def __init__(self, sock: WebSocket, nick: str | None = None):
     if nick is None:
       nick = "Player"
 
-    self.id = get_player_id()
+    self.id = next(player_id)
     self.nick = nick
     self.sock = sock
 
-  async def send(self, message):
+  async def send(self, message: bytes):
     if self.sock is not None:
       await self.sock.send_bytes(message)
 
 class Room:
-  thread: threading.Thread
-  running: bool
-  id: int
+  # thread: threading.Thread
+  task: asyncio.Task | None
   emulator: Emulator
+
+  id: int
   players: list[Player]
   inputs: dict[int,str]
 
   def __init__(self, rom: RomPrivate):
-    self.running = True
-    self.id = get_room_id()
+    self.task = None
     self.emulator = Emulator(rom.filename)
+
+    self.id = next(room_id)
     self.players = []
     self.inputs = {}
 
@@ -80,25 +82,19 @@ class Room:
   def player_leave(self, player: Player):
     self.players.remove(player)
 
-  def run(self):
-    self.emulator.start_game()
-    self.thread = threading.Thread(target = asyncio.run, args = (self.run_internal(), ))
-    self.thread.start()
+  # def run(self):
+  #   self.thread = threading.Thread(target = asyncio.run, args = (self.run_internal(), ))
+  #   self.thread.start()
 
-  async def run_internal(self):
-    while self.running:
-      await self.tick()
+  # async def run_internal(self):
+  #   while self.running:
+  #     await self.tick()
 
   def send_command(self):
     value, count =  Counter(self.inputs.values()).most_common(1)[0]
     if count == 0:
       return
     self.emulator.send_button(value)
-
-  async def tick(self):
-    self.emulator.tick()
-    frame = self.emulator.get_frame()
-    await self.broadcast([b'F'] + frame)
 
   async def broadcast(self, message):
     for player in self.players:
@@ -107,47 +103,78 @@ class Room:
   def reset_input(self):
     self.inputs = {}
 
-  def stop(self):
-    self.running = False
-    self.thread.join()
+  def start(self):
+    if self.task is not None:
+      return
 
-rooms: list[Room] = []
+    loop = asyncio.get_event_loop()
+
+    self.emulator.start()
+    self.task = loop.create_task(self._loop())
+
+  def stop(self):
+    if self.task is None:
+      return
+
+    self.task.cancel()
+    self.emulator.stop()
+
+  async def _loop(self):
+    try:
+      loop = asyncio.get_running_loop()
+
+      while True:
+        print("Tick ", end="", flush=True)
+
+        # with concurrent.futures.ProcessPoolExecutor() as pool:
+          # await loop.run_in_executor(pool, self.emulator.tick, ())
+
+        self.emulator.tick()
+        self.emulator.update_framebuffer()
+
+        print(self.emulator.framebuffer, flush=True)
+
+        for player in self.players:
+          await player.send(b'F')
+          await player.send(self.emulator.framebuffer)
+
+        await asyncio.sleep(1/10)
+
+        print("Tock", flush=True)
+    finally:
+      pass
+
+rooms: dict[int, Room] = {}
 
 class RoomCreate(BaseModel):
   rom_id: str
 
 @router.get("/rooms", response_model=list[int])
 async def get_rooms() -> list[int]:
-  return [room.id for room in rooms]
+  return [room.id for room in rooms.values()]
 
 @router.post("/rooms")
 async def create_room(body: RoomCreate):
   room = Room(roms[body.rom_id])
-  rooms.append(room)
-  room.run()
+  rooms[room.id] = room
+  room.start()
   return room.id
-
-# TODO: not this bullshit
-@router.put("/rooms/{room_id}/{player_id}/{input}")
-async def send_input(room_id: int, player_id: int, input: str):
-  room = rooms[[room.id for room in rooms].index(room_id)]
-  room.inputs[player_id] = input
 
 @router.websocket("/ws/{room_id}")
 async def websocket(sock: WebSocket, room_id: int):
-  if room_id not in [room.id for room in rooms]:
+  room = rooms[room_id]
+  if room is None:
     await sock.close()
     return
-  room = rooms[[room.id for room in rooms].index(room_id)]
 
   await sock.accept()
   player = Player(sock)
+  player.nick = await sock.receive_text()
 
-  
-  player.nick = str(await sock.receive_text())
   room.player_join(player)
+
   print(f"Player named {player.nick} joined room {room_id}.")
-  await player.send(f"Player named {player.nick} joined room {room_id}.")
+  await player.send(str.encode(f"Player named {player.nick} joined room {room_id}."))
 
   try:
       while True:
